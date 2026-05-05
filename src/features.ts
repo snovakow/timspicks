2edd1724b447b37bb16718881e23b1987e54751e
snovakow <snovakow@gmail.com>
2026-05-05 15:39:56 -0400

    correlation feature
    Add feature variable

--- src/features.ts
+++ src/features.ts
@@ -1,4 +1,5 @@
 export const normalizeSportsbooks = false;
+export const allPlayersTable = false;
+export const correlation = false;
 export const simulate = false;
 export const analyze = false;
-export const allPlayersTable = false;
